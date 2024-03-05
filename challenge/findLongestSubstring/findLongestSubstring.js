1438f310949e2712c011b79baa03d2324a9711b8
JongDeug <jongdeug2021@gmail.com>
2024-03-05 17:24:29 +0900

    3/5 복습 큐 추가

--- challenge/findLongestSubstring/findLongestSubstring.js
+++ challenge/findLongestSubstring/findLongestSubstring.js
@@ -21,30 +21,30 @@
 // [문제 세분화]
 // str 받아서 알파벳이 단 한번만 존재해야하는 가장 긴 문자열의 길이 반환하는 함수 구현
 function findLongestSubstring(str) {
-    // 필요한 변수를 생성해
+    // 필요한 변수 3가지
     let longest = 0;
     let seen = {};
     let start = 0;
 
-    // 루프를 돌아
     for (let i = 0; i < str.length; i++) {
-        // 문자를 하나 받아
-        let char = str[i];
+        // character 받기
+        let chr = str[i];
 
-        // seen에 char이 있으면?
-        if (seen[char]) {
-            // start와 seen[char]을 비교해
-            start = Math.max(start, seen[char]);
+        // 본 것 중에 있는거면 start 이동
+        if (seen[chr] >= 0) {
+            // start = Math.max(start, seen[chr] + 1); // max를 하는 이유가?
+            start = seen[chr] + 1;
         }
 
-        // longest를 구해
-        longest = Math.max(longest, i - start + 1);
+        // longest 할당
+        longest = Math.max(longest, i - start + 1); // 길이니까 + 1 추가
 
-        // seen[char]이 없으면? i+1을 해
-        seen[char] = i + 1;
+        // 본 것에 추가
+        seen[chr] = i;
     }
     return longest;
 }
 
-// console.log(findLongestSubstring('rithmschool'))
-findLongestSubstring('rithmschool')
+console.log(findLongestSubstring('rithmschool'));
+console.log(findLongestSubstring('bbbbbb'));
+console.log(findLongestSubstring('thisisawesome'));
