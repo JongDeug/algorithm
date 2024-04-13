a6db5dd96f6b7fa166ed01122be5605d2c47a86a
JongDeug <jongdeug2021@gmail.com>
2024-04-13 15:20:37 +0900

    4/13 조이스틱 조금있다 이해해야

---
+++ challenge/programmers/greedy/조이스틱.js
@@ -0,0 +1,20 @@
+// [문제 이해하기]
+// 주어진 name을 완성하기 위한 최소 조이스틱 조작 횟수를 return하는 함수 구현
+
+// 입력: name(string)
+// 출력: int(최솟값)
+
+// 조건
+// 1. 위 : 다음 알파벳
+// 2. 아래 : 이전 알파벳(범위)
+// 3. 왼쪽 : 커서 왼쪽 이동(범위)
+// 4. 오른쪽 : 커서 오른쪽 이동(범위)
+// 5. 커서는 왼쪽에 항상 있음.
+
+// 어떻게 구현할지 감이 안옴 ㅠㅠ
+// https://velog.io/@a_in/Programmers-Greedy-Level-2-Joystick-JavaScript
+
+function solution(name) {
+    var answer = 0;
+    return answer;
+}
