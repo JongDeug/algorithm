ff85da6ada4520cd2dea0397115df437ec2e42e5
JongDeug <jongdeug2021@gmail.com>
2024-06-03 22:57:41 +0900

    부분 조합 구하는 함수 추가

--- snippets/permutation/permutation-without-repetition.js
+++ snippets/permutation/permutation-without-repetition.js
@@ -39,8 +39,8 @@ const permutationWithDepth = (depth, tmp) => {
     }
 };
 
-// M. check 배열을 사용하지 않는 방법
-const permutationWithoutCheck = (list, tmp) => {
+// M. check 배열을 사용하지 않는 방법 1 : O(n * n!)
+const permutationWithoutCheck1 = (list, tmp) => {
     if (tmp.length === 2) {
         ans.push([...tmp]);
         return;
@@ -52,12 +52,30 @@ const permutationWithoutCheck = (list, tmp) => {
         // const remain = list.slice(0, i).concat(list.slice(i + 1)); // list.slice(0,0) => [] 임
         // I. filter 사용 => 이게 더 괜찮네
         const remain = list.filter((v, idx) => idx !== i);
-        permutationWithoutCheck(remain, tmp);
+        permutationWithoutCheck1(remain, tmp);
+        tmp.pop();
+    }
+};
+
+// I. check 배열을 사용하지 않는 방법 2 : O(n * n!), 1 방법 보다 공간 복잡도 측면에서 조금 더 좋음
+// 하지만 check 배열을 쓰는게 불필요한 검사를 더 줄일 수 있음.
+const permutationWithoutCheck2 = (tmp) => {
+    if (tmp.length === 2) { // 원하는 순열 길이
+        ans.push([...tmp]);
+        return;
+    }
+
+    for (let i = 0; i < input.length; i++) {
+        if (tmp.includes(input[i])) continue; // 이미 사용된 요소는 건너뛴다.
+
+        tmp.push(input[i]);
+        permutationWithoutCheck2(tmp); // 다음 깊이로 재귀 호출
         tmp.pop();
     }
 };
 
 // permutationWithTmp([]);
 // permutationWithDepth(0, []);
-permutationWithoutCheck(input, []);
+// permutationWithoutCheck1(input, []);
+permutationWithoutCheck2([]);
 console.log(ans);
