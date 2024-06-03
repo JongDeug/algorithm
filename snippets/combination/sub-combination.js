5d5e1e8d3efa0cf9b2d645c11e150613642ed686
JongDeug <jongdeug2021@gmail.com>
2024-06-03 22:58:27 +0900

    부분 조합 구하는 함수 추가

--- snippets/combination/sub-combination.js
+++ snippets/combination/sub-combination.js
@@ -2,6 +2,7 @@ const input = Array.from({ length: 10 }, (v, i) => i + 1);
 let ans = [];
 
 // M. 부분 조합을 구하는 함수
+// [1,2,3,4] 중 부분 조합을 구하는 코드임
 const subCombination = (depth, tmp) => {
     if (depth === 4) {
         ans.push([...tmp]);
