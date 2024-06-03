264c3354231a12be70003de8b1bd14f2cb5ad047
JongDeug <jongdeug2021@gmail.com>
2024-06-03 22:37:19 +0900

    순열 조합 추가

--- snippets/combination/combination.js
+++ snippets/combination/combination.js
@@ -15,5 +15,25 @@ const combinationWithTmp = (depth, tmp) => {
     }
 };
 
-combinationWithTmp(0, []);
+// M. for 문 없이 조합을 만드는 법
+const combinationWithoutFor = (depth, tmp) => {
+    if (tmp.length === 2) { // I. 원하는 조합 길이
+        ans.push([...tmp]);
+        return;
+    }
+
+    if (depth === input.length) { // I. 더 이상 선택할 요소가 없을 때
+        return;
+    }
+
+    tmp.push(input[depth]);
+    combinationWithoutFor(depth, tmp); // I. 중복 포함이므로 depth 만 넘김
+    tmp.pop();
+
+    // I. depth 올리는 용도 (for 문 대체)
+    combinationWithoutFor(depth + 1, tmp);
+};
+
+// combinationWithTmp(0, []);
+combinationWithoutFor(0, []);
 console.log(ans);
