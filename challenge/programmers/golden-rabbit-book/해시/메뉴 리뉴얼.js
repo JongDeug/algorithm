8f371abd08c4fbab71035e5f596c76f9452ac338
JongDeug <jongdeug2021@gmail.com>
2024-09-11 13:48:07 +0900

    코딩딩 테스트 합격자 되기 8일차

--- challenge/programmers/golden-rabbit-book/해시/메뉴 리뉴얼.js
+++ challenge/programmers/golden-rabbit-book/해시/메뉴 리뉴얼.js
@@ -1,6 +1,9 @@
 // [문제 이해하기]
 // 주문 목록을 보고 단품세트를 만드는 로직을 짜라.
 //
+// [조건]
+// 주문할 때 가장 많이 함께 주문한 단품메뉴들을 코스요리 메뉴로 구성하기로 했다.
+//
 // [문제 세분화]
 // function solution(orders, course) {
 //   let answer = new Set();
@@ -42,28 +45,50 @@
 //   console.log(answer);
 // }
 
+// [문제 세분화] => 아니 ...
 function solution(orders, course) {
+  let answer = [];
   // I. course와 orders 를 순회하면서 조합을 만들어야 함.
   course.forEach((num) => {
     let bucket = [];
 
     orders.forEach((order) => {
-      let splitOrder = order.split(" ");
+      // I. 이놈이 정렬이 안된 놈임
+      let splitOrder = order.split("").sort();
       // M. 조합 함수 구현
       const combination = (tmp, depth) => {
-        if (tmp.length === n) {
+        if (tmp.length === num) {
           bucket.push([...tmp]);
           return;
         }
         for (let i = depth; i < splitOrder.length; i++) {
           if (tmp.includes(splitOrder[i])) continue;
           tmp.push(splitOrder[i]);
-          combination(tmp, depth + 1);
+          combination(tmp, i + 1);
           tmp.pop();
         }
       };
 
-      combination(splitOrder, num, []);
+      combination([], 0);
     });
+
+    const counter = new Map();
+    bucket.forEach((item) => {
+      const str = item.join("");
+      counter.set(str, (counter.get(str) || 0) + 1);
+    });
+
+    const max = Math.max(...counter.values());
+    // I. 최소 2명이 선택
+    if (max > 1) {
+      for (const [key, value] of counter) {
+        // I. 최대이지만 같은 경우도 포함함
+        if (value === max) {
+          answer.push(key);
+        }
+      }
+    }
   });
+
+  return answer.sort();
 }
