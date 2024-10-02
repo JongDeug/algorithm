0ee168cf699f75881334c8b25f543625413052d3
JongDeug <jongdeug2021@gmail.com>
2024-10-02 14:36:18 +0900

    코딩 테스트 합격자 되기 21일차

--- challenge/golden-rabbit-book/백트래킹/양궁 대회.js
+++ challenge/golden-rabbit-book/백트래킹/양궁 대회.js
@@ -10,42 +10,109 @@
 // I. N을 기준으로 라이언의 순열(점수)를 구함
 // i. BASE CASE (순열을 다 구하면 stop => 계산)
 // i. max 를 넣음 만약 값이 같다면 ? => 더 많이 맞힌 경우를 return, 값이 없다면? [-1]
+// function solution(n, info) {
+//   const lionInfo = new Array(11).fill(0);
+//   let maxScoreDiff = -Infinity;
+//   let result = [-1];
+//
+//   const search = (depth, sum) => {
+//     // I. Base Case
+//     if (depth === 11) {
+//       if (sum === n) {
+//         let [lionScore, apeachScore] = [0, 0];
+//         // I. 누가 누가 더 크냐
+//         for (let i = 0; i <= 10; i++) {
+//           const score = 10 - i;
+//           const [lionN, apeachN] = [lionInfo[i], info[i]];
+//           // I. 0 제외
+//           if (apeachN && apeachN >= lionN) apeachScore += score;
+//           else if (apeachN < lionN) lionScore += score;
+//         }
+//
+//         if (apeachScore < lionScore) {
+//           const scoreDiff = lionScore - apeachScore;
+//
+//           if (maxScoreDiff <= scoreDiff) {
+//             if (maxScoreDiff === scoreDiff) {
+//               for (let i = 10; i >= 0; i--) {
+//                 // I. 와..습니ㅏㅇ렁ㄴㄹ result가 커도 종료해줘야지
+//                 if (result[i] > lionInfo[i]) break; // 와... 이걸 처리안해줬네
+//
+//                 if (result[i] < lionInfo[i]) {
+//                   result = [...lionInfo];
+//                   break;
+//                 }
+//               }
+//             } else {
+//               maxScoreDiff = scoreDiff;
+//               result = [...lionInfo];
+//             }
+//           }
+//         }
+//         return;
+//       }
+//       return;
+//     }
+//
+//     // I. N을 기준으로 순열을 구한다
+//     for (let i = 1; i <= n; i++) {
+//       if (sum + i > n) break;
+//       lionInfo[depth] = i;
+//       search(depth + 1, sum + i);
+//       lionInfo[depth] = 0; // sum 은 원상태이므로 빼지 않아도 됨
+//     }
+//
+//     search(depth + 1, sum);
+//   };
+//
+//   search(0, 0);
+//   console.log(maxScoreDiff);
+//   return result;
+// }
+
+// [피드백]
 function solution(n, info) {
   const lionInfo = new Array(11).fill(0);
   let maxScoreDiff = -Infinity;
   let result = [-1];
 
+  const calculateScore = (lion, apeach) => {
+    let [lionScore, apeachScore] = [0, 0];
+
+    for (let i = 0; i <= 10; i++) {
+      const score = 10 - i;
+      // I. 0 제외
+      if (!apeach[i] && !lion[i]) continue;
+      if (apeach[i] >= lion[i]) apeachScore += score;
+      else lionScore += score;
+    }
+
+    return [lionScore, apeachScore];
+  };
+
   const search = (depth, sum) => {
     // I. Base Case
     if (depth === 11) {
       if (sum === n) {
-        let [lionScore, apeachScore] = [0, 0];
-        // I. 누가 누가 더 크냐
-        for (let i = 0; i <= 10; i++) {
-          const score = 10 - i;
-          const [lionN, apeachN] = [lionInfo[i], info[i]];
-          // I. 0 제외
-          if (apeachN && apeachN >= lionN) apeachScore += score;
-          else if (apeachN < lionN) lionScore += score;
-        }
+        // I. 점수 계산
+        let [lionScore, apeachScore] = calculateScore(lionInfo, info);
 
+        // I. 변수에 넣기
         if (apeachScore < lionScore) {
           const scoreDiff = lionScore - apeachScore;
 
-          if (maxScoreDiff <= scoreDiff) {
-            if (maxScoreDiff === scoreDiff) {
-              for (let i = 10; i >= 0; i--) {
-                // I. 와..습니ㅏㅇ렁ㄴㄹ result가 커도 종료해줘야지
-                if (result[i] > lionInfo[i]) break; // 와... 이걸 처리안해줬네
-
-                if (result[i] < lionInfo[i]) {
-                  result = [...lionInfo];
-                  break;
-                }
+          if (maxScoreDiff < scoreDiff) {
+            console.log(lionInfo);
+            maxScoreDiff = scoreDiff;
+            result = [...lionInfo]; // 참조 값
+          } else if (maxScoreDiff === scoreDiff) {
+            for (let i = 10; i >= 0; i--) {
+              // I. 중요 : result 가 커도 종료, 같을 경우만 루프를 돔
+              if (result[i] > lionInfo[i]) break;
+              if (result[i] < lionInfo[i]) {
+                result = [...lionInfo]; // 참조 값
+                break;
               }
-            } else {
-              maxScoreDiff = scoreDiff;
-              result = [...lionInfo];
             }
           }
         }
@@ -59,14 +126,13 @@ function solution(n, info) {
       if (sum + i > n) break;
       lionInfo[depth] = i;
       search(depth + 1, sum + i);
-      lionInfo[depth] = 0; // sum 은 원상태이므로 빼지 않아도 됨
+      lionInfo[depth] = 0;
     }
 
     search(depth + 1, sum);
   };
 
   search(0, 0);
-  console.log(maxScoreDiff);
   return result;
 }
 
