b9cb96660a4ac3029f2d04213fe53d25c2305bfd
JongDeug <jongdeug2021@gmail.com>
2024-11-05 12:31:21 +0900

    코딩 테스트 합격자 되기 복습큐 4일차

--- challenge/golden-rabbit-book/해시/메뉴 리뉴얼.js
+++ challenge/golden-rabbit-book/해시/메뉴 리뉴얼.js
@@ -1,88 +1,90 @@
-// [문제 이해하기]
-// 주문 목록을 보고 단품세트를 만드는 로직을 짜라.
-//
-// [조건]
-// 주문할 때 가장 많이 함께 주문한 단품메뉴들을 코스요리 메뉴로 구성하기로 했다.
-//
-// [문제 세분화]
+// [정답 코드]
 // function solution(orders, course) {
-//   let answer = new Set();
-//
-//   // I. 알파벳 카운트
-//   const alphabetCount = new Map();
-//   orders.forEach((str) => {
-//     [...str].forEach((chr) => {
-//       alphabetCount.set(chr, (alphabetCount.get(chr) || 0) + 1);
-//     });
-//   });
-//
-//   // M. subSet 구하는 함수
-//   const subSet = (depth, tmp, bucket) => {
-//     if (depth === bucket.length) {
-//       if (tmp.length > 0) {
-//         answer.add(tmp.join(""));
-//       }
-//       return;
-//     }
-//
-//     tmp.push(bucket[depth]);
-//     subSet(depth + 1, tmp, bucket);
-//     tmp.pop();
-//
-//     subSet(depth + 1, tmp, bucket);
-//   };
-//
-//   // I. course 기준 알파벳 뽑음 => 뽑은 알파벳으로 조합
+//   let answer = [];
+//   // I. course와 orders 를 순회하면서 조합을 만들어야 함.
 //   course.forEach((num) => {
 //     let bucket = [];
-//     for (const item of alphabetCount) {
-//       const [key, value] = item;
-//       if (value >= num) bucket.push(key);
+
+//     orders.forEach((order) => {
+//       // I. 이놈이 정렬이 안된 놈임
+//       let splitOrder = order.split("").sort();
+//       // M. 조합 함수 구현
+//       const combination = (tmp, depth) => {
+//         if (tmp.length === num) {
+//           bucket.push([...tmp]);
+//           return;
+//         }
+//         for (let i = depth; i < splitOrder.length; i++) {
+//           if (tmp.includes(splitOrder[i])) continue;
+//           tmp.push(splitOrder[i]);
+//           combination(tmp, i + 1);
+//           tmp.pop();
+//         }
+//       };
+
+//       combination([], 0);
+//     });
+
+//     const counter = new Map();
+//     bucket.forEach((item) => {
+//       const str = item.join("");
+//       counter.set(str, (counter.get(str) || 0) + 1);
+//     });
+
+//     const max = Math.max(...counter.values());
+//     // I. 최소 2명이 선택
+//     if (max > 1) {
+//       for (const [key, value] of counter) {
+//         // I. 최대이지만 같은 경우도 포함함
+//         if (value === max) {
+//           answer.push(key);
+//         }
+//       }
 //     }
-//     subSet(0, [], bucket);
 //   });
 //
-//   console.log(answer);
+//   return answer.sort();
 // }
 
-// [문제 세분화] => 아니 ...
+// [문제 이해하기] => 정답 코드, 이게 더 깔끔한듯?
+// 코스 어쩌고 저쩌고,,, 코스 요리 메뉴를 구성하기로 함. 적절한 코스 메뉴는?
+
+// [문제 풀이하기]
+// course에 있는 숫자를 가지고 orders에서 조합을 구함
+// orders를 돌면서 구한 조합이 있는지 체크+카운팅(해시)
+// 해시 카운트 정렬 후 상위 두 놈 뺌. if 카운트가 2이상이 아니거나 조합 자체가 없으면 추가하지 않음
+// 결과는 정렬을 한 상태에서 줘야함.
 function solution(orders, course) {
   let answer = [];
-  // I. course와 orders 를 순회하면서 조합을 만들어야 함.
+  // course -> orders 순서로
   course.forEach((num) => {
-    let bucket = [];
+    const counter = new Map();
 
     orders.forEach((order) => {
-      // I. 이놈이 정렬이 안된 놈임
-      let splitOrder = order.split("").sort();
-      // M. 조합 함수 구현
-      const combination = (tmp, depth) => {
+      order = order.split("").sort();
+
+      const combination = (depth, tmp) => {
         if (tmp.length === num) {
-          bucket.push([...tmp]);
+          // 이잉씨 여기서 나는 order를 또 돌라했음. 하지만 조합에서 나온 것이 곧 정답임
+          const joinTmp = tmp.join("");
+          counter.set(joinTmp, (counter.get(joinTmp) || 0) + 1);
           return;
         }
-        for (let i = depth; i < splitOrder.length; i++) {
-          if (tmp.includes(splitOrder[i])) continue;
-          tmp.push(splitOrder[i]);
-          combination(tmp, i + 1);
+
+        for (let i = depth; i < order.length; i++) {
+          tmp.push(order[i]);
+          combination(i + 1, tmp);
           tmp.pop();
         }
       };
 
-      combination([], 0);
-    });
-
-    const counter = new Map();
-    bucket.forEach((item) => {
-      const str = item.join("");
-      counter.set(str, (counter.get(str) || 0) + 1);
+      combination(0, []);
     });
 
     const max = Math.max(...counter.values());
-    // I. 최소 2명이 선택
-    if (max > 1) {
+    // 두 명 이상이 뽑았는가?
+    if (max >= 2) {
       for (const [key, value] of counter) {
-        // I. 최대이지만 같은 경우도 포함함
         if (value === max) {
           answer.push(key);
         }
@@ -90,5 +92,5 @@ function solution(orders, course) {
     }
   });
 
-  return answer.sort();
+  return answer;
 }
--- challenge/golden-rabbit-book/해시/할인 행사.js
+++ challenge/golden-rabbit-book/해시/할인 행사.js
@@ -6,28 +6,28 @@
 
 // [문제 세분화]
 function solution(want, number, discount) {
-    let answer = 0;
+  let answer = 0;
 
-    // I. discout.length - 9 => 값 이전 까지 탐색
-    for (let i = 0; i < discount.length - 9; i++) {
-        // I. want, number 합친 obj 생성
-        let dataObj = {};
-        for (let i = 0; i < want.length; i++) {
-            dataObj[want[i]] = number[i];
-        }
-
-        // i. obj에서 값을 뺀다.
-        discount.slice(i, i + 10).forEach((item) => {
-            if (dataObj[item]) dataObj[item] -= 1;
-        });
-        // i. obj 값이 0이 아닌 놈이 있으면 answer x
-        let flag = true;
-        for (const key in dataObj) {
-            if (dataObj[key] !== 0) flag = false;
-        }
-        // i. 다 0이면 answer ++
-        if (flag) answer++;
+  // I. discout.length - 9 => 값 이전 까지 탐색
+  for (let i = 0; i < discount.length - 9; i++) {
+    // I. want, number 합친 obj 생성
+    let dataObj = {};
+    for (let i = 0; i < want.length; i++) {
+      dataObj[want[i]] = number[i];
     }
 
-    return answer;
+    // i. obj에서 값을 뺀다.
+    discount.slice(i, i + 10).forEach((item) => {
+      if (dataObj[item]) dataObj[item] -= 1;
+    });
+    // i. obj 값이 0이 아닌 놈이 있으면 answer x
+    let flag = true;
+    for (const key in dataObj) {
+      if (dataObj[key] !== 0) flag = false;
+    }
+    // i. 다 0이면 answer ++
+    if (flag) answer++;
+  }
+
+  return answer;
 }
