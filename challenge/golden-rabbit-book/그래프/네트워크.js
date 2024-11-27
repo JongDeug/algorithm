c6f7ba5011ca841324f95032b93946b9c1a53804
JongDeug <jongdeug2021@gmail.com>
2024-11-27 12:09:28 +0900

    코딩 테스트 합격자 되기 복습큐 14일차

--- challenge/golden-rabbit-book/그래프/네트워크.js
+++ challenge/golden-rabbit-book/그래프/네트워크.js
@@ -40,45 +40,101 @@
 //   return newSet.size;
 // }
 
-// [문제 세분화] => 1. DFS
+// // [문제 세분화] => 1. DFS
+// function solution(n, computers) {
+//   let answer = 0;
+//   const visited = new Array(n).fill(false);
+
+//   const DFS = (node) => {
+//     visited[node] = true;
+
+//     for (let i = 0; i < n; i++) {
+//       if (!visited[i] && computers[node][i]) {
+//         // 방문 x && 1인경우
+//         DFS(i);
+//       }
+//     }
+//   };
+
+//   for (let i = 0; i < n; i++) {
+//     if (!visited[i]) {
+//       // 이걸로 컨트롤하는구나
+//       DFS(i);
+//       answer++;
+//     }
+//   }
+
+//   return answer;
+// }
+
+// console.log(
+//   solution(3, [
+//     [1, 1, 0],
+//     [1, 1, 0],
+//     [0, 0, 1],
+//   ]),
+// );
+
+// console.log(
+//   solution(3, [
+//     [1, 1, 0],
+//     [1, 1, 1],
+//     [0, 1, 1],
+//   ]),
+// );
+
+// [문제 이해하기]
+// 네트워크 개수를 return
+
+// [입력]: int(노드 수), 이차원배열(인접행렬)
+// [출력]: int(네트워크 개수)
+
+// [접근법]
+// 상호배타 집합을 생각해봤는데 사이클 때문에 적절하지 않음.
+// DFS 선택
+
+// [문제 세분화]
+// 인접 행렬 => 인접 리스트
+// node의 개수만큼 for문
+// 만약 visited 하지 않았다면
+// 	DFS 구현 => while, 방문처리는 들어왔을 때
+// 	answer++;
 function solution(n, computers) {
   let answer = 0;
-  const visited = new Array(n).fill(false);
+  const visited = new Set();
+  const adjacencyList = {};
 
-  const DFS = (node) => {
-    visited[node] = true;
+  // 행렬 => 리스트
+  for (let i = 0; i < computers.length; i++) {
+    adjacencyList[i] = [];
+    for (let j = 0; j < computers[0].length; j++) {
+      if (i !== j && computers[i][j] === 1) adjacencyList[i].push(j);
+    }
+  }
 
-    for (let i = 0; i < n; i++) {
-      if (!visited[i] && computers[node][i]) {
-        // 방문 x && 1인경우
-        DFS(i);
+  // DFS 구현
+  const dfs = (node) => {
+    const stack = [node];
+
+    while (stack.length) {
+      const u = stack.pop();
+      visited.add(u);
+
+      for (const neighbor of adjacencyList[u]) {
+        if (!visited.has(neighbor)) {
+          stack.push(neighbor);
+        }
       }
     }
   };
 
+  // node 개수만큼 반복
   for (let i = 0; i < n; i++) {
-    if (!visited[i]) {
-      // 이걸로 컨트롤하는구나
-      DFS(i);
+    if (!visited.has(i)) {
+      dfs(i);
       answer++;
     }
   }
 
   return answer;
 }
-
-console.log(
-  solution(3, [
-    [1, 1, 0],
-    [1, 1, 0],
-    [0, 0, 1],
-  ]),
-);
-
-console.log(
-  solution(3, [
-    [1, 1, 0],
-    [1, 1, 1],
-    [0, 1, 1],
-  ]),
-);
