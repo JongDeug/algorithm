0698e6c9fccfcc2432b2ec0a663d7545fa2c6321
JongDeug <jongdeug2021@gmail.com>
2024-12-03 14:04:26 +0900

    코딩 테스트 합격자 되기 복습큐 20일차

---
+++ challenge/golden-rabbit-book/백트래킹/test.js
@@ -0,0 +1,65 @@
+// [문제 이해하기]
+// 외벽의 취약 지점을 점검하기 위해 보내야 하는 최소 친구 수 return
+
+// [입력]: int(n, 외벽 길이), arr(weak, 취약 지점), arr(dist, 친구 이동 가능 거리)
+// [출력]: int(최소 친구 수)
+// - 만약 전부 점검할 수 없는 경우 -1 return
+
+// [접근법]
+// 각 취약 지점 + n 해서 한 바퀴 돌 수 있는 긴 배열을 만든다.
+// dist를 내림차순으로 정렬한 뒤 천천히 돌아본다.
+
+// [문제 세분화]
+// min값 정의
+// weak 길이 만큼 for문 (i, 기준점)
+// 	while i를 기준점으로 사용, j를 count로 사용, j는 dist의 idx
+// 		arr[i] + dist[j] < arr[i+1]
+//			=> i += 1, 다음 취약점으로
+//		arr[i] + dist[j] >= arr[i+1]
+//			=> 다른 취약점으로
+//		j += 1;
+//	min = Math.min(min, j);
+// 만약 min이 weak 배열 길이면 return -1
+function solution(n, weak, dist) {
+  // min 값
+  let answer = Infinity;
+  // 긴 배열 생성
+  const arr = [...weak];
+  weak.forEach((v) => arr.push(v + n));
+  // 내림차순으로 정렬
+  dist = dist.sort((a, b) => b - a);
+
+  // weak 길이만큼 반복
+  for (let i = 0; i < weak.length; i++) {
+    let startIdx = i;
+    let j = 0; // count로 사용, dist idx로 사용
+    const visited = new Set();
+
+    while (
+      startIdx < arr.length &&
+      j < dist.length &&
+      visited.size < weak.length
+    ) {
+      const sum = arr[startIdx] + dist[j];
+      visited.add(startIdx);
+      if (sum < arr[startIdx + 1]) startIdx++;
+      else {
+        // sum이 큰 경우 적절한 위치(idx)로 이동해야
+        for (let k = startIdx + 1; k < arr.length; k++) {
+          visited.add(k);
+          if (sum < arr[k]) {
+            startIdx = k;
+            break;
+          }
+        }
+      }
+      j++; // count up
+    }
+    answer = Math.min(answer, j);
+  }
+
+  return answer > dist.length ? -1 : answer;
+}
+
+console.log(solution(12, [1, 3, 4, 9, 10], [3, 5, 7]));
+console.log(solution(12, [1, 5, 6, 10], [1, 2, 3, 4]));
