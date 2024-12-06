b82dd6f95174da367b7f9c54993c27e3e8e9003e
JongDeug <jongdeug2021@gmail.com>
2024-12-06 13:35:46 +0900

    코딩 테스트 합격자 되기 복습큐 23일차

--- challenge/golden-rabbit-book/그리디/구명보트.js
+++ challenge/golden-rabbit-book/그리디/구명보트.js
@@ -35,20 +35,48 @@
 // 처리하면 3으로 나옴..
 // console.log(solution([20, 30, 70, 80], 100)); //
 
+// function solution(people, limit) {
+//   let answer = 0;
+//   let [i, j] = [0, people.length - 1];
+//   people.sort((a, b) => a - b);
+//   while (i <= j) {
+//     if (people[i] + people[j] <= limit) {
+//       i++;
+//     }
+//     // 짝 맞춰서 결과나온거 아닌 이상 마지막 요소(0인덱스)에서 +1 해줘야됨
+//     answer++;
+//     j--;
+//   }
+//   return answer;
+// }
+
+// // console.log(solution([20, 30, 70, 80], 100)); //
+// // console.log(solution([60, 70, 80], 100)); //
+// console.log(solution([10, 30, 50, 50], 240)); //
+
+// [문제 이해하기]
+// 구명보트를 최대한 적게 사용해서 모든 사람을 구출해!
+
+// [입력]: arr(people, 몸무게), int(limit, 구명보트 무게 제한)
+// [출력]: int(구명보트 개수의 최솟값)
+
+// [접근법]
+// [30, 50, 50, 70, 80] 이라 가정하고,
+// 정렬 후 처음부터 처리하면 => 4
+// 짝을 맞추면 => 3
+
+// [문제 세분화]
+// 아쑤삐 최대 2명이네 아..1.11!1
+// 문제를 잘 읽읍시다
 function solution(people, limit) {
   let answer = 0;
+  people = people.sort((a, b) => a - b);
   let [i, j] = [0, people.length - 1];
-  people.sort((a, b) => a - b);
   while (i <= j) {
-    if (people[i] + people[j] <= limit) {
-      i++;
-    }
-    // 짝 맞춰서 결과나온거 아닌 이상 마지막 요소(0인덱스)에서 +1 해줘야됨
-    answer++;
+    const sum = people[i] + people[j];
+    if (sum <= limit) i++;
     j--;
+    answer++;
   }
   return answer;
 }
-
-// console.log(solution([20, 30, 70, 80], 100)); //
-console.log(solution([60, 70, 80], 100)); //
