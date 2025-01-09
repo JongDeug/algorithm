cb59e3c930290879d534c137a982e77b1849f362
JongDeug <jongdeug2021@gmail.com>
2025-01-09 13:31:15 +0900

    복습 큐 배열 회전하기

--- challenge/golden-rabbit-book/시뮬레이션/배열 회전하기.js
+++ challenge/golden-rabbit-book/시뮬레이션/배열 회전하기.js
@@ -1,76 +1,57 @@
 // [문제 이해하기]
-// 행렬을 시계방향으로 90도 * n번 돌리라.
-//
-// [문제 풀이]
-// 시계방향 90도 => A[i,j] = A[j, (N-1)-j]
-// 반시계방향 90도 => A[i,j] = A[(N-1)-j, j]
-//
-// [문제 세분화]
-// I. 2차원 배열을 새롭게 담을 변수를 작성해야지
-// I. 공식대로 작성하면됨
-// function solution(arr, n) {
-//   const len = arr.length;
-//   const answer = Array.from({ length: len }, () => Array(len).fill(0));
-//
-//   // I. 반복문
-//   for (let t = 0; t < n; t++) {
-//     for (let i = 0; i < len; i++) {
-//       for (let j = 0; j < len; j++) {
-//         answer[j][len - 1 - i] = arr[i][j];
-//       }
-//     }
-//     arr = answer.map((x) => [...x]);
-//   }
-//
-//   return answer;
-// }
+// 행렬을 시계 방향으로 90도, n번 돌린 값을 반환해라. 
 
-// [피드백] => 분할하는 것이 좋다.
+// [접근법]
+// 시계 방향: [i,j] => [j, (N-1)-i] (시계 방향이니까 오른쪽 변경으로 외우기)
+// 반시계 방향: [i,j] => [(N-1)-j, i] (반시계 방향이니까 왼쪽 변경으로 외우기)
+
+// [문제 세분화] => 동작을 분할하는 것이 좋다.
+// 1. 공식을 가지고 90도씩 회전하는 함수
+// 2. 함수를 n번 활용해서 답을 반환한다
 function solution(arr, n) {
-  // I. 90도 시계방향 회전
-  const rotate90 = (rotated) => {
-    const len = arr.length;
-    const answer = Array.from({ length: len }, () => Array(len).fill(0));
+    let answer = arr.map(x => [...x]);
+    const arrLen = arr.length;
 
-    for (let i = 0; i < len; i++) {
-      for (let j = 0; j < len; j++) {
-        answer[j][len - 1 - i] = rotated[i][j];
-      }
+    const rotate = (array) => {
+        // 새로 만들기 귀찮아서
+        const rotated = array.map(x => [...x]);
+
+        for (let i = 0; i < arrLen; i++) {
+            for (let j = 0; j < arrLen; j++) {
+                rotated[j][arrLen - 1 - i] = array[i][j];
+            }
+        }
+
+        return rotated;
+    };
+
+    for (let k = 0; k < n; k++) {
+        answer = rotate(answer);
     }
 
     return answer;
-  };
-
-  let rotated = arr.map((x) => [...x]);
-
-  // I. n번 돌림
-  for (let t = 0; t < n; t++) {
-    rotated = rotate90(rotated);
-  }
-
-  return rotated;
 }
 
 console.log(
-  solution(
-    [
-      [1, 2, 3, 4],
-      [5, 6, 7, 8],
-      [9, 10, 11, 12],
-      [13, 14, 15, 16],
-    ],
-    1,
-  ),
+    solution(
+        [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16],
+        ],
+        1,
+    ),
 );
 
 console.log(
-  solution(
-    [
-      [1, 2, 3, 4],
-      [5, 6, 7, 8],
-      [9, 10, 11, 12],
-      [13, 14, 15, 16],
-    ],
-    2,
-  ),
+    solution(
+        [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16],
+        ],
+        2,
+    ),
 );
